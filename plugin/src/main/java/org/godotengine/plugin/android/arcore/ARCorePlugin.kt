0426777a7bdb60ac65decbc059738c4b702399bd
Luca Junge <LucaJunge@posteo.de>
2024-08-02 16:27:29 +0200

    ARCore returns hit test results

--- plugin/src/main/java/org/godotengine/plugin/android/arcore/ARCorePlugin.kt
+++ plugin/src/main/java/org/godotengine/plugin/android/arcore/ARCorePlugin.kt
@@ -3,10 +3,13 @@ package org.godotengine.plugin.android.arcore
 
 import android.Manifest
 import android.app.Activity
+import android.content.Context
 import android.content.pm.PackageManager
 import android.util.Log
 import android.view.Choreographer
+import android.view.Display
 import android.view.View
+import android.view.WindowManager
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -14,32 +17,43 @@ import com.google.ar.core.ArCoreApk
 import com.google.ar.core.Camera
 import com.google.ar.core.Config
 import com.google.ar.core.Frame
+import com.google.ar.core.HitResult
+import com.google.ar.core.Pose
 import com.google.ar.core.Session
 import com.google.ar.core.exceptions.CameraNotAvailableException
-import com.google.ar.core.exceptions.UnavailableUserDeclinedInstallationException
+import com.google.ar.core.exceptions.TextureNotSetException
 import com.google.ar.core.exceptions.UnavailableDeviceNotCompatibleException
+import com.google.ar.core.exceptions.UnavailableUserDeclinedInstallationException
 import org.godotengine.godot.Godot
 import org.godotengine.godot.gl.GLSurfaceView
+import org.godotengine.godot.gl.GodotRenderer
 import org.godotengine.godot.plugin.GodotPlugin
 import org.godotengine.godot.plugin.UsedByGodot
-
-/*
-* Currently you have to start the app twice, as the permission flow is currently not working,
-* search for @PermissionError in this file for explanation.
-* Start the app once for accepting the permissions and a second time for creating the ARCore session
-* and (trying to) update the frame
-* */
+import java.nio.IntBuffer
+import javax.microedition.khronos.egl.EGLConfig
+import javax.microedition.khronos.opengles.GL10
 
 class ARCorePlugin(godot: Godot): GodotPlugin(godot) {
+
+    /**
+     * The surfaceView is passed to the Sample Renderer in HelloArActivity.java...
+     * We need to call setRenderer (and pass a renderer?)
+     * Change the pixel format?
+     * For now: setRenderMode to on-demand?
+     */
 
     companion object {
         val TAG = ARCorePlugin::class.java.simpleName
         var requestARCoreInstall: Boolean = false
         const val CAMERA_REQUEST_CODE: Int = 100
-        //
         var session : Session? = null
-        var choreographer = Choreographer.getInstance()
-        private val surfaceView: GLSurfaceView? = null
+        private var surfaceView: GLSurfaceView? = null
+        private var hasSetTextureNames: Boolean = false
+        private var cameraTextureId: Int = -1
+        private var textureBuffer: IntBuffer? = null
+        private var viewportWidth: Int = -1
+        private var viewportHeight: Int = -1
+        private var viewportChanged: Boolean = false
 
         init {
             try {
@@ -53,6 +67,11 @@ class ARCorePlugin(godot: Godot): GodotPlugin(godot) {
 
     override fun onMainCreate(activity: Activity?): View? {
         super.onMainCreate(activity)
+        //windowManager = activity!!.getSystemService(Context.WINDOW_SERVICE) as WindowManager?
+        surfaceView = GLSurfaceView(activity)
+        // I need to give a GLSurfaceView.Renderer and somehow, implementing the interface
+        // in an own class does not seem to be accepted...
+        surfaceView!!.setRenderer(GodotRenderer())
 
         // Check if all requirements for installation are met => (XR mode), Camera access and ARCore support
         if(arCoreRequirementsSatisfied()) {
@@ -68,8 +87,13 @@ class ARCorePlugin(godot: Godot): GodotPlugin(godot) {
     // Called once at the start and upon every reopening of the app after
     // sending it to the background e.g. when installing ARCore from the Play Store
     override fun onMainResume() {
-        Log.v(TAG, "onMainResume")
         super.onMainResume()
+
+        // Note that order matters - see the note in onPause(), the reverse applies here.
+        if(session != null) {
+            session!!.resume()
+            surfaceView!!.onResume()
+        }
 
         try {
             when(ArCoreApk.getInstance().requestInstall(activity, requestARCoreInstall)) {
@@ -81,27 +105,29 @@ class ARCorePlugin(godot: Godot): GodotPlugin(godot) {
         } catch (e: UnavailableDeviceNotCompatibleException) {
             Toast.makeText(activity, "Please install ARCore to use this app", Toast.LENGTH_LONG).show()
         }
-
-        /*if(session != null)  {
-            session!!.resume()
-        }
-
-        if(surfaceView != null) {
-            surfaceView.onResume()
-        }*/
     }
 
     override fun onMainPause() {
         super.onMainPause()
         if(session != null) {
+            // Note that the order matters - GLSurfaceView is paused first so that it does not try
+            // to query the session. If Session is paused before GLSurfaceView, GLSurfaceView may
+            // still call session.update() and get a SessionPausedException.
             surfaceView!!.onPause()
             session!!.pause()
         }
     }
 
     override fun onMainDestroy() {
+        if(session != null) {
+            // Explicitly close ARCore Session to release native resources.
+            // Review the API reference for important considerations before calling close() in apps with
+            // more complicated lifecycle requirements:
+            // https://developers.google.com/ar/reference/java/arcore/reference/com/google/ar/core/Session#close()
+            session!!.close()
+            session = null
+        }
         super.onMainDestroy()
-        session!!.close()
     }
 
     private fun arCoreRequirementsSatisfied(): Boolean {
@@ -138,8 +164,8 @@ class ARCorePlugin(godot: Godot): GodotPlugin(godot) {
 
     fun startARCore() {
     Log.v(TAG, "in startARCore")
-        // Create a session here etc.
-        session = createSession()   
+        // Create a session to use
+        session = createSession()
     }
 
     override fun onMainRequestPermissionsResult(
@@ -153,7 +179,7 @@ class ARCorePlugin(godot: Godot): GodotPlugin(godot) {
                 if(grantResults!!.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     Log.v(TAG, "ARCore: Camera permission was granted")
                     // Permission is granted. Continue the action or workflow in your app
-                    // TODO: How to continue the workflow here?
+                    // TODO: How to correctly continue the workflow here? For now just starting ARCore
                     startARCore()
                 }
             } else -> {
@@ -178,30 +204,76 @@ class ARCorePlugin(godot: Godot): GodotPlugin(godot) {
         session!!.configure(config)
         Log.v(TAG, session.toString())
 
-        // Start the frame loop for debug here
-        //startFrameUpdates()
         return session
     }
 
-    private fun startFrameUpdates() {
-        choreographer.postFrameCallback { frameTimeNanoSeconds ->
-            updateARCoreFrame()
+    override fun onGLSurfaceChanged(gl: GL10?, width: Int, height: Int) {
+        super.onGLSurfaceChanged(gl, width, height)
+        onSurfaceChanged(width, height)
+    }
 
-            choreographer.postFrameCallback { this }
+    override fun onGLSurfaceCreated(gl: GL10?, config: EGLConfig?) {
+        super.onGLSurfaceCreated(gl, config)
+        textureBuffer = IntBuffer.wrap(intArrayOf(100*100))
+        gl!!.glGenTextures(1, textureBuffer )
+        gl.glBindTexture(GL10.GL_TEXTURE_2D, cameraTextureId)
+    }
+
+    fun updateSessionIfNeeded(session: Session) {
+        if (viewportChanged) {
+            //val displayRotation: Int = display.rotation
+            // hardcoded values for the display width and height here for now...
+            session.setDisplayGeometry(0, 1080, 2400)
+            viewportChanged = false
         }
     }
 
-    private fun updateARCoreFrame() {
-        val frame: Frame
-        try {
-            frame = session!!.update()
-        } catch (e: CameraNotAvailableException) {
-            Log.e(TAG, "Camera not available during onDrawFrame", e)
+    override fun onGLDrawFrame(gl: GL10?) {
+
+        // Do we call the super up here or later?
+        super.onGLDrawFrame(gl)
+
+        if(session == null) {
             return
         }
-        val camera: Camera = frame.getCamera()
-        Log.v(TAG, frame.toString())
-        Log.v(TAG, camera.toString())
+
+        updateSessionIfNeeded(session!!)
+
+        if (!hasSetTextureNames) {
+            session!!.setCameraTextureNames(intArrayOf(cameraTextureId))
+            hasSetTextureNames = true;
+        }
+
+        var frame: Frame;
+
+        try {
+            var displayRotation: Int = 0 //display.rotation
+            session!!.setDisplayGeometry(displayRotation, viewportWidth, viewportHeight)
+
+            frame = session!!.update()
+
+            var hitResultList: List<HitResult>  = frame.hitTest(100.0F, 100.0F)
+
+            if(hitResultList.isNotEmpty()) {
+                var nearestHit = hitResultList[0]
+
+                var distanceToCamera: Float = nearestHit.distance
+                var pose: Pose = nearestHit.hitPose
+                Log.v(TAG, "Distance from camera to hit: $nearestHit")
+                Log.v(TAG, "Pose of the hit: $pose")
+            }
+
+        } catch(e: CameraNotAvailableException) {
+            Log.e(TAG, "ARCorePlugin: Camera was not available during onGLDrawFrame", e)
+        } catch(e: TextureNotSetException) {
+            Log.v(TAG, "ARCorePlugin: The texture was not set")
+        }
+    }
+
+    fun onSurfaceChanged(width: Int, height: Int) {
+        viewportWidth = width
+        viewportHeight = height
+        viewportChanged = true
     }
 
     override fun getPluginName() = BuildConfig.GODOT_PLUGIN_NAME
