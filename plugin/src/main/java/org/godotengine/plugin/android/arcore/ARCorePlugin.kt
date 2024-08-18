46206cd2042b0619ff23a18bcc4c124d2d36ec35
Luca Junge <LucaJunge@posteo.de>
2024-08-18 10:29:45 +0200

    Setup flow now also works when ARCore needs to be installed

--- plugin/src/main/java/org/godotengine/plugin/android/arcore/ARCorePlugin.kt
+++ plugin/src/main/java/org/godotengine/plugin/android/arcore/ARCorePlugin.kt
@@ -10,6 +10,7 @@ import com.google.ar.core.ArCoreApk
 import com.google.ar.core.Config
 import com.google.ar.core.Frame
 import com.google.ar.core.HitResult
+import com.google.ar.core.Plane
 import com.google.ar.core.Pose
 import com.google.ar.core.Session
 import com.google.ar.core.exceptions.CameraNotAvailableException
@@ -29,9 +30,14 @@ import javax.microedition.khronos.opengles.GL10
 class ARCorePlugin(godot: Godot): GodotPlugin(godot) {
 
     companion object {
-        val TAG = ARCorePlugin::class.java.simpleName
+        val TAG = ARCorePlugin::class.java.simpleName // TAG = "ARCorePlugin"
 
-        var requestARCoreInstall: Boolean = false
+        // Installation and Setup Flow
+        var userRequestedInstall: Boolean = false
+        var arCoreInstalled: Boolean = false
+        var arCoreSupported: Boolean = false
+        var cameraPermissionGranted : Boolean = false
+
         var session : Session? = null
         private var surfaceView: GLSurfaceView? = null
         private var hasSetTextureNames: Boolean = false
@@ -43,7 +49,7 @@ class ARCorePlugin(godot: Godot): GodotPlugin(godot) {
 
         init {
             try {
-                Log.v(TAG, "Loading ${BuildConfig.GODOT_PLUGIN_NAME} library")
+                Log.d(TAG, "Loading ${BuildConfig.GODOT_PLUGIN_NAME} library")
                 System.loadLibrary(BuildConfig.GODOT_PLUGIN_NAME)
             } catch (e: UnsatisfiedLinkError) {
                 Log.e(TAG, "Unable to load ${BuildConfig.GODOT_PLUGIN_NAME} shared library")
@@ -53,12 +59,34 @@ class ARCorePlugin(godot: Godot): GodotPlugin(godot) {
 
     override fun onMainCreate(activity: Activity): View? {
         super.onMainCreate(activity)
-        PermissionsUtil.requestManifestPermissions(activity)
+        Log.d(TAG, "onMainCreate()")
 
-        //windowManager = activity!!.getSystemService(Context.WINDOW_SERVICE) as WindowManager?
+        // 1. Check if ARCore is available and/or can be installed
+        Log.d(TAG, "checkAvailability")
+        when(ArCoreApk.getInstance().checkAvailability(activity)) {
+            ArCoreApk.Availability.SUPPORTED_APK_TOO_OLD, ArCoreApk.Availability.SUPPORTED_NOT_INSTALLED -> {
+                Log.d(TAG, "ARCoreApk.Availability: SUPPORTED_NOT_INSTALLED or SUPPORTED_APK_TOO_OLD. Please install or update ARCore")
+                arCoreSupported = true
+            }
+            ArCoreApk.Availability.SUPPORTED_INSTALLED -> {
+                Log.d(TAG, "ARCoreApk.Availability: SUPPORTED_INSTALLED")
+                arCoreInstalled = true
+                arCoreSupported = true
+            }
+            else -> {
+                Log.d(TAG, "ARCoreApk.Availability: Not SUPPORTED_*")
+                Toast.makeText(activity, "ARCore support can't be determined, AR features probably won't work", Toast.LENGTH_LONG).show()
+            }
+            // the else branch represents all these (non)-availabilities:
+            /*ArCoreApk.Availability.UNKNOWN_CHECKING -> Log.d(TAG, "ARCoreApk.Availability: UNKNOWN_CHECKING")
+            ArCoreApk.Availability.UNKNOWN_ERROR -> Log.d(TAG, "ARCoreApk.Availability: UNKNOWN_ERROR")
+            ArCoreApk.Availability.UNKNOWN_TIMED_OUT -> Log.d(TAG, "ARCoreApk.Availability: UNKNOWN_TIMED_OUT")
+            ArCoreApk.Availability.UNSUPPORTED_DEVICE_NOT_CAPABLE -> Log.d(TAG, "ARCoreApk.Availability: UNSUPPORTED_DEVICE_NOT_CAPABLE")*/
+        }
+
+        // These are placeholders needed to get ARCore to report hit tests
+        // Will be removed later
         surfaceView = GLSurfaceView(activity)
-        // I need to give a GLSurfaceView.Renderer and somehow, implementing the interface
-        // in an own class does not seem to be accepted...
         surfaceView!!.setRenderer(GodotRenderer())
 
         return null
@@ -68,29 +96,72 @@ class ARCorePlugin(godot: Godot): GodotPlugin(godot) {
     // sending it to the background e.g. when installing ARCore from the Play Store
     override fun onMainResume() {
         super.onMainResume()
+        Log.d(TAG, "onMainResume")
 
-        // Note that order matters - see the note in onPause(), the reverse applies here.
-        if(session != null) {
-            session!!.resume()
-            surfaceView!!.onResume()
-        }
-
-        try {
-            when(ArCoreApk.getInstance().requestInstall(activity, requestARCoreInstall)) {
-                ArCoreApk.InstallStatus.INSTALL_REQUESTED -> requestARCoreInstall = false
-                else -> Unit
+        if(arCoreSupported && !arCoreInstalled) {
+            try {
+                when(ArCoreApk.getInstance().requestInstall(activity, userRequestedInstall)) {
+                    ArCoreApk.InstallStatus.INSTALLED -> {
+                        Log.d(TAG, "ARCore is installed. Requesting camera permission now")
+                        arCoreSupported = true
+                        arCoreInstalled = true
+                    }
+                    ArCoreApk.InstallStatus.INSTALL_REQUESTED -> userRequestedInstall = true
+                }
+            } catch (e: UnavailableUserDeclinedInstallationException) {
+                Toast.makeText(activity, "UnavailableUserDeclinedInstallationException: Please install ARCore to use this app", Toast.LENGTH_LONG).show()
+                Log.d(TAG, "UnavailableUserDeclinedInstallationException: Please install ARCore to use this app")
+                //userRequestedInstall = false
+                // How should the setup flow continue here?
+            } catch (e: UnavailableDeviceNotCompatibleException) {
+                Toast.makeText(activity, "UnavailableDeviceNotCompatibleException: Your device is not compatible with ARCore", Toast.LENGTH_LONG).show()
+                Log.d(TAG, "UnavailableDeviceNotCompatibleException: Your device is not compatible with ARCore")
+                // How should the setup flow continue here?
+                //userRequestedInstall = false
             }
-        } catch (e: UnavailableUserDeclinedInstallationException) {
-            Toast.makeText(activity, "Please install ARCore to use this app", Toast.LENGTH_LONG).show()
-        } catch (e: UnavailableDeviceNotCompatibleException) {
-            Toast.makeText(activity, "Please install ARCore to use this app", Toast.LENGTH_LONG).show()
         }
+
+        // Check if the camera permission is already granted or not
+        cameraPermissionGranted = checkCameraPermission()
+
+        // ARCore is supported and installed, but the camera permission is not yet granted
+        if((arCoreSupported && arCoreInstalled) && !cameraPermissionGranted) {
+            Log.d(TAG, "Requesting camera permissions")
+            PermissionsUtil.requestManifestPermissions(activity)
+        }
+
+        // Everything set up, create or resume session
+        if(arCoreSupported && arCoreInstalled && cameraPermissionGranted) {
+            if (session == null) {
+                session = createSession()
+                session!!.resume()
+            } else {
+                // Note that order of resuming matters - see the note in onMainPause(), the reverse applies here.
+                session!!.resume()
+                surfaceView!!.onResume()
+            }
+        }
+    }
+
+    fun checkCameraPermission(): Boolean {
+        Log.d(TAG, "checkCameraPermission")
+        val grantedPermissions: Array<String> = PermissionsUtil.getGrantedPermissions(activity)
+        if(grantedPermissions.isNotEmpty()) {
+            grantedPermissions.forEach { permission ->
+                if (permission == "android.permission.CAMERA") {
+                    return true
+                }
+            }
+        }
+        return false
     }
 
     override fun onMainPause() {
         super.onMainPause()
+        Log.d(TAG, "onMainPause()")
+
         if(session != null) {
-            // Note that the order matters - GLSurfaceView is paused first so that it does not try
+            // Note that the order of pausing matters - GLSurfaceView is paused first so that it does not try
             // to query the session. If Session is paused before GLSurfaceView, GLSurfaceView may
             // still call session.update() and get a SessionPausedException.
             surfaceView!!.onPause()
@@ -99,6 +170,7 @@ class ARCorePlugin(godot: Godot): GodotPlugin(godot) {
     }
 
     override fun onMainDestroy() {
+        Log.d(TAG, "onMainDestroy")
         if(session != null) {
             // Explicitly close ARCore Session to release native resources.
             // Review the API reference for important considerations before calling close() in apps with
@@ -110,39 +182,33 @@ class ARCorePlugin(godot: Godot): GodotPlugin(godot) {
         super.onMainDestroy()
     }
 
-    fun startARCore() {
-    Log.v(TAG, "in startARCore")
-        if(session == null) {
-            // Create a session to use
-            session = createSession()
-        }
-    }
-
     override fun onMainRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>?,
         grantResults: IntArray?
     ) {
         super.onMainRequestPermissionsResult(requestCode, permissions, grantResults)
-        //Log.v(TAG, requestCode.toString()) // "1001" -> comes from PermissionUtil
+        //Log.d(TAG, requestCode.toString()) // "1001" -> comes from PermissionUtil
 
         permissions?.forEachIndexed { index, permission ->
-            Log.v(TAG, "ARCorePlugin: Permission ${permission} is ${if (grantResults?.get(index) == PackageManager.PERMISSION_GRANTED) "granted" else "not granted"}")
-            // We only have one permission (CAMERA) in the manifest, start ARCore if it is granted...
-            startARCore()
+            // Check if the camera permission was granted and resume in onMainResume()
+            if(permission.equals("android.permission.CAMERA") && grantResults?.get(index) == PackageManager.PERMISSION_GRANTED) {
+                Log.d(TAG, "Camera permission is granted")
+                cameraPermissionGranted = true
+            }
         }
     }
 
     // Creates a session and configures it (https://developers.google.com/ar/develop/java/session-config)
     fun createSession(): Session {
-        var session = Session(activity)
+        val session = Session(activity)
         val config = Config(session)
 
         // Do feature-specific operations here, such as enabling depth or turning on
         // support for Augmented Faces
 
-        session!!.configure(config)
-        Log.v(TAG, session.toString())
+        session.configure(config)
+        //Log.d(TAG, session.toString())
 
         return session
     }
@@ -169,6 +235,7 @@ class ARCorePlugin(godot: Godot): GodotPlugin(godot) {
     }
 
     override fun onGLDrawFrame(gl: GL10?) {
+        //Log.d(TAG,"onGLDrawFrame")
 
         // Do we call the super up here or later?
         super.onGLDrawFrame(gl)
@@ -184,13 +251,23 @@ class ARCorePlugin(godot: Godot): GodotPlugin(godot) {
             hasSetTextureNames = true;
         }
 
-        var frame: Frame;
+        var frame: Frame
+        var trackables: Collection<Plane>
 
         try {
             var displayRotation: Int = 0 //display.rotation
             session!!.setDisplayGeometry(displayRotation, viewportWidth, viewportHeight)
 
             frame = session!!.update()
+
+            trackables = frame.getUpdatedTrackables(Plane::class.java)
+            var resultString = ""
+            if (trackables.isNotEmpty()) {
+                trackables.forEach() { trackable ->
+                    resultString += "$trackable, "
+                }
+                Log.d(TAG, resultString)
+            }
 
             var hitResultList: List<HitResult>  = frame.hitTest(100.0F, 100.0F)
 
@@ -199,14 +276,14 @@ class ARCorePlugin(godot: Godot): GodotPlugin(godot) {
 
                 var distanceToCamera: Float = nearestHit.distance
                 var pose: Pose = nearestHit.hitPose
-                Log.v(TAG, "Distance from camera to hit: $distanceToCamera")
-                Log.v(TAG, "Pose of the hit: $pose")
+                Log.d(TAG, "Distance from camera to hit: $distanceToCamera")
+                Log.d(TAG, "Pose of the hit: $pose")
             }
 
         } catch(e: CameraNotAvailableException) {
             Log.e(TAG, "ARCorePlugin: Camera was not available during onGLDrawFrame", e)
         } catch(e: TextureNotSetException) {
-            Log.v(TAG, "ARCorePlugin: The texture was not set")
+            Log.d(TAG, "ARCorePlugin: The texture was not set")
         }
     }
 
